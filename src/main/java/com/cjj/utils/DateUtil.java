41ba0a327e7f54146c8695ec8528c11195dff289
changjiajia <2245741634@qq.com>
2020-02-11 18:26:41 +0800

    工具类

---
+++ src/main/java/com/cjj/utils/DateUtil.java
@@ -0,0 +1,129 @@
+package com.cjj.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * @ClassName: DateUtil
+ * @Description: 日期工具类
+ * @author changjiajia
+ *
+ */
+public class DateUtil {
+
+	private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+	// 根据生日计算年龄
+	public static int getAgeByBirthday(Date birthday) {
+		// 获得日历控件
+		Calendar calendar = Calendar.getInstance();
+		// 从日历控件中获得当前的年月日
+		int year = calendar.get(Calendar.YEAR);
+		int month = calendar.get(Calendar.MONTH);
+		int day = calendar.get(Calendar.DAY_OF_MONTH);
+		// 把生日赋给日历控件 方便操作
+		calendar.setTime(birthday);
+		int birYear = calendar.get(Calendar.YEAR);
+		int birMonth = calendar.get(Calendar.MONTH);
+		int birDay = calendar.get(Calendar.DAY_OF_MONTH);
+		int age = year - birYear;
+		// 如果当前月份大于生日月份 年龄+1
+		if (month > birMonth) {
+			age++;
+		}
+		// 月份相等如果当前日期大于生日日期 年龄+1
+		if (birMonth == month && day > birDay) {
+			age++;
+		}
+		return age;
+	}
+
+	// 方法的重载 计算年龄
+	public static int getAgeByBirthday(String birthday) {
+		Date birDate = null;
+		try {
+			birDate = sdf.parse(birthday);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		return getAgeByBirthday(birDate);
+	}
+
+	// 计算两个日期相差天数
+	public static int getDayNum(Date date1, Date date2) {
+		// 一天多少毫秒
+		Long time = 24L * 60 * 60 * 1000;
+		// 获得结束时间的毫秒
+		long time1 = date2.getTime();
+		// 获得开始时间的毫秒
+		long time2 = date1.getTime();
+		// 两者相减 = 相差的毫秒
+		// 相差的毫秒/一天多少毫秒 = 天
+		if (time1 > time2) {
+			return (int) ((time1 - time2) / time);
+		} else {
+			return (int) ((time2 - time1) / time);
+		}
+	}
+
+	// 方法的重载 计算两个日期相差天数
+	public static int getDayNum(String date1, String date2) {
+		try {
+			return getDayNum(sdf.parse(date1), sdf.parse(date2));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+	// 根据给定的时间 获得当月的第一天的0时0分0秒 2019-12-12 == 2012-12-1 0:0:0
+	public static Date getDateByInitMouth(Date date) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);
+		// 当当前时间的日历控件的日期设置成1
+		calendar.set(Calendar.DAY_OF_MONTH, 1);
+		calendar.set(Calendar.HOUR_OF_DAY, 0);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.SECOND, 0);
+		// 返回设置之后的时间
+		return calendar.getTime();
+	}
+
+	// 方法的重载 根据给定的时间 获得当月的第一天的0时0分0秒
+	public static Date getDateByInitMouth(String date) {
+		try {
+			return getDateByInitMouth(sdf.parse(date));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	// 获得当月的最后一秒
+	public static Date getDateByEndMouth(Date date) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date);// 2012-9-10
+		// 当前月份+1
+		calendar.add(Calendar.MONTH, 1);// 2012-10-10
+		// 通过调用上面的方法 获得当月开始时间
+		Date dateByInitMouth = getDateByInitMouth(calendar.getTime());// 2012-10-1 0:0:0
+		calendar.setTime(dateByInitMouth);
+		calendar.add(Calendar.SECOND, -1);
+		return calendar.getTime();
+
+	}
+
+	// 方法的重载 根据给定的时间 获得当月的第一天的0时0分0秒
+	public static Date getDateByEndMouth(String date) {
+		try {
+			return getDateByEndMouth(sdf.parse(date));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
---
+++ src/main/java/com/cjj/utils/StringUtil.java
@@ -0,0 +1,60 @@
+package com.cjj.utils;
+
+/**
+ * 
+ * @ClassName: StringUtil
+ * @Description: 字符串工具类
+ * @author: changjiajia
+ */
+public class StringUtil {
+	// 判断是否是数字
+	public static boolean isNumber(String str) {
+		String reg_str = "^([1-9][0-9]*)|((([0-9])|([1-9]\\d+))\\.\\d)$";
+		boolean matches = str.matches(reg_str);
+		return matches;
+	}
+
+	// 判断源字符串是否有值，空引号也算没值
+	public static boolean isNull(String str) {
+		if (null != str && !"".equals(str)) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	public static boolean isNotNull(String str) {
+		if (null != str && !"".equals(str)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	// 判断源字符串是否有值，空引号和空格也算没值
+	public static boolean isNotEmpty(String str) {
+		if (null != str && !"".equals(str)) {
+			if (str.trim().length() == 0) {
+				return false;
+			}
+			return true;
+		}
+		return false;
+	}
+
+	// 验证手机号码
+	public static boolean isPhone(String str) {
+		// 正则 1 3456789 [abc]
+		String reg_str = "^1[3-9]\\d{9}$";
+		boolean matches = str.matches(reg_str);
+		return matches;
+	}
+
+	// 验证邮箱
+	public static boolean isEmail(String str) {
+		// 正则 154565@qq.com lqd@163.cn net com.cn
+		String reg_str = "^\\w+@$\\w+\\.(com|cn|net|com\\.cn)";
+		boolean matches = str.matches(reg_str);
+		return matches;
+	}
+}
